36410815f6d95688a3acfe321f3bfe10e7fca227
VoidWatcher <VoidWatcher@users.noreply.github.com>
2017-07-22 11:45:51 +0800

    section-1 completed

--- practice/section-1/practice-3-spec.js
+++ practice/section-1/practice-3-spec.js
@@ -4,7 +4,9 @@
 describe('practice-1-3', () => {
 
   const collectionA = ['a', 'e', 'h', 't', 'f', 'c', 'g', 'b', 'd'];
-  const collectionB = {value: ['a', 'd', 'e', 'f']};
+  const collectionB = {
+    value: ['a', 'd', 'e', 'f']
+  };
 
   it('选出A集合中跟B对象中value属性中的元素相同的元素', () => {
 
